12a6a63bde467df948bef15e0d17e47ba836b897
unknown <louis.martin-compiegne@hetic.net>
2020-03-13 13:42:31 +0100

    form

---
+++ views/register.php
@@ -0,0 +1,17 @@
+<?php ?>
+<html>
+    <form action="post">
+        <label for="login">Login</label>
+        <input type="text" name="login">
+        <label for="">Password</label>
+        <input type="password">
+        <label for="lastname">Nom</label>
+        <input type="text" name="lastname">
+        <label for="firstname">Prenom</label>
+        <input type="text" name="lastname">
+        <label for="photo"></label>
+        <input type="file" accept="image/png, image/jpeg">
+        <input type="submit" value="subscribe">
+    </form>
+</html>
+
